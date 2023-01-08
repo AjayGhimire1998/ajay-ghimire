7b4fb0446a439d06a40939320a6cc4271c022cb3
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-08 20:27:16 +1100

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
-
+      <div>
+        <img src={logo} alt="logo"/>
+        <h1>hello</h1>
+      </div>
     </div>
   );
 }
