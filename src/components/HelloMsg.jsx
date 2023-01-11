dc6da97f97352ba3423c1ed26b41cc6ed9c89ca9
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-11 20:24:39 +1100

    adding styles to typewriter

--- src/components/HelloMsg.jsx
+++ src/components/HelloMsg.jsx
@@ -2,17 +2,28 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function HelloMsg() {
+  var app = document.getElementsByClassName("hello-world");
+
+  let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+  });
+  console.log("Hello,world");
+
   return (
-    <div className="hello-world">
+    <div className="hello-world" style={{ fontSize: "30px" }}>
       <Typewriter
         options={{
           strings: [
-            'System.out.println("Hello World !");',
-            'console.log("Hello World !")',
-            'puts("Hello World !"',
+            '<span style="color:#cc6600">System</span>.<span style="color:#00ccff">out</span>.println("Hello World !");',
+            'console.log("Hello World !");',
+            'puts "Hello World !"',
           ],
+          words: ["hello"],
           autoStart: true,
           loop: true,
+          //   skipAddStyles: true,
+          deleteSpeed: 50,
         }}
       />
     </div>
