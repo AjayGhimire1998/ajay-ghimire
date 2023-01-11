c9154318b86adb81b7af1f2d53d9b3552bd6a9d6
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-11 20:39:02 +1100

    adding styles to typewriter; fixed

--- src/components/HelloMsg.jsx
+++ src/components/HelloMsg.jsx
@@ -2,28 +2,19 @@ import React from "react";
 import Typewriter from "typewriter-effect";
 
 function HelloMsg() {
-  var app = document.getElementsByClassName("hello-world");
-
-  let typewriter = new Typewriter(app, {
-    loop: true,
-    delay: 75,
-  });
-  console.log("Hello,world");
-
   return (
     <div className="hello-world" style={{ fontSize: "30px" }}>
       <Typewriter
         options={{
           strings: [
-            '<span style="color:#cc6600">System</span>.<span style="color:#00ccff">out</span>.println("Hello World !");',
-            'console.log("Hello World !");',
-            'puts "Hello World !"',
+            '<span style="color:#cc6600">System</span>.<span style="color:#00ccff"><b>out</b></span>.<span style="color:#99ff33">println</span>(<span style="color:#00cc00">"Hello World !"</span>);',
+            '<span style="color:#00ccff">console</span>.<span style="color:#ffff00">log</span><span style="color:#ff00ff">(</span><span style="color:#ff9900 ">"Hello World !"</span><span style="color:#ff00ff">)</span>;',
+            '<span style="color:#ffff00">puts</span> <span style="color:#ff9900 ">"Hello World !"</span>',
           ],
           words: ["hello"],
           autoStart: true,
           loop: true,
-          //   skipAddStyles: true,
-          deleteSpeed: 50,
+          deleteSpeed: 10,
         }}
       />
     </div>
