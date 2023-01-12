a528dd7fa2d131f542673f44f21e46c26fd3a245
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-12 20:08:14 +1100

    idk whats wrong with the browser

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
-import HelloMsg from '../components/HelloMsg'
+import React from "react";
+import Logo from "../components/static/Logo";
+import HelloMsg from "../components/static/HelloMsg";
 
 function Home() {
   return (
     <>
+      <div className="ui inverted vertical masthead center aligned segment">
+        <Logo />
         <HelloMsg />
+      </div>
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
