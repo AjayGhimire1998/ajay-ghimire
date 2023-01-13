16656a452df69f6b16e038373788071a1f3e927e
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-13 21:28:24 +1100

    added wave

--- src/components/home/HelloMsg.jsx
+++ src/components/home/HelloMsg.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 import "./hello-message.css"
+import wave from "./wave.gif"
 
 function HelloMsg() {
   return (
     <div className="typewriter">
+      
       <Typewriter
         options={{
           strings: [
-            '<span style="color:#cc6600">System</span>.<span style="color:#00ccff"><b>out</b></span>.<span style="color:#99ff33">println</span>(<span style="color:#00cc00">"Hello World !"</span>);',
-            '<span style="color:#00ccff">console</span>.<span style="color:#ffff00">log</span><span style="color:#ff00ff">(</span><span style="color:#cc6600 ">"Hello World !"</span><span style="color:#ff00ff">)</span>;',
-            '<span style="color:#ffff00">puts</span> <span style="color:#ff9900 ">"Hello World !"</span>',
+            `<span style="color:#cc6600">System</span>.<span style="color:#00ccff"><b>out</b></span>.<span style="color:#99ff33">println</span>(<span style="color:#00cc00">"Hello World !"</span>);`,
+            `<span style="color:#00ccff">console</span>.<span style="color:#ffff00">log</span><span style="color:#ff00ff">(</span><span style="color:#cc6600 ">"Hello World !"</span><span style="color:#ff00ff">)</span>;`,
+            `<span style="color:#ffff00">puts</span> <span style="color:#ff9900 ">"Hello World !"</span>`,
           ],
           autoStart: true,
           loop: true,
           deleteSpeed: 25,
         }}
       />
+      <img src={wave} alt="wave" className="wave"/>
     </div>
   );
 }
