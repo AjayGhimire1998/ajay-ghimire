9f14cda2fd665afa0bd966bd947ce14be4b7d9b5
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-16 23:15:51 +1100

    done for today

--- src/components/about/about-contents/Quote.jsx
+++ src/components/about/about-contents/Quote.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import "./quote.css";
+import ajay1 from "../../../images/my-pictures/ajay1.jpeg";
 function Quote() {
   return (
     <div className="quote">
       <h4>
         <blockquote>
-          <i>
-            “I have gone from slicing
-            sashimis to slicing
-            arrays”
-          </i>
+          <i>“I have gone from slicing sashimis to slicing arrays”</i>
         </blockquote>
       </h4>
+      {/* <img src={ajay1} style={{ imageRendering: "pixelated", height: "200px", width: "300px" }} alt="ajay" /> */}
     </div>
   );
 }
