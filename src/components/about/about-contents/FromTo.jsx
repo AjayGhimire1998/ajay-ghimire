a13b9c844743697ddbb0f8e2e9b72f69804cf070
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-18 22:23:20 +1100

    working on timeline

--- src/components/about/about-contents/FromTo.jsx
+++ src/components/about/about-contents/FromTo.jsx
@@ -3,23 +3,26 @@ import "./from-to.css";
 function FromTo() {
   return (
     <div className="from-to-container">
-      <h3 style={{textAlign: "center"}}>Timeline</h3>
-      
-      <div className="education-container">
-        <div className="education">
-          <h3>edu</h3>
-          <h3>edu</h3>
-          <h3>edu</h3>
-          <h3>edu</h3>
+      <h3 style={{ textAlign: "center" }}>
+        <span>Timeline</span>
+      </h3>
+      <div className="edu-exp-container">
+        <div className="edu-container">
+          <div className="edu">
+            <h3>edu</h3>
+            <h3>edu</h3>
+            <h3>edu</h3>
+            <h3>edu</h3>
+          </div>
         </div>
-      </div>
-      <div className="line"></div>
-      <div className="exp-container">
-        <div className="exp">
-          <h3>exp</h3>
-          <h3>exp</h3>
-          <h3>exp</h3>
-          <h3>exp</h3>
+        <div className="line"></div>
+        <div className="exp-container">
+          <div className="exp">
+            <h3>exp</h3>
+            <h3>exp</h3>
+            <h3>exp</h3>
+            <h3>exp</h3>
+          </div>
         </div>
       </div>
     </div>
