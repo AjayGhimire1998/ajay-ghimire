54bdafe7be6b8e2ee4141d784c59b9718882b8b9
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-21 00:58:59 +1100

    cleaned code

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./about-me.css";
+import { aboutData } from "./aboutData.js";
 import AboutItem from "./about-contents/AboutItem";
 import profile from "../../images/my-pictures/profile.jpeg";
 import background from "../../images/my-pictures/chocho.JPG";
@@ -18,19 +19,9 @@ function AboutMe({ shadow, play, hover }) {
 
   const pages = [
     <AboutItem
-      title="Profile"
-      desc="I am a highly motivated and results-driven software developer with a 
-      strong background in IT and a passion for creating innovative
-      solutions. I have recently graduated with a Bachelor's
-      degree in IT and completed a software engineering bootcamp.
-      I have proven ability to quickly learn and 
-      master new technologies, and a track record of delivering
-      high-quality web applications on time."
-      desc2="I have strong problem-solving skills and ability
-      to work well in a team environment.
-      I am seeking a challenging software developer role 
-      where I can utilize my skills and continue to grow
-      as a professional, along with actively learning 'Java'."
+      title={aboutData[0].title}
+      desc={aboutData[0].desc}
+      desc2={aboutData[0].desc2}
       image={profile}
       isPreviewOn={isPreviewOn}
       onPreviewClick={onPreviewClick}
@@ -38,16 +29,9 @@ function AboutMe({ shadow, play, hover }) {
       shadow={shadow}
     />,
     <AboutItem
-      title="Professional Evolution"
-      desc="During my bachelor's,
-      I started working as a kitchen hand in a super busy Japanese restaurant
-      to support my tuition. 
-      My hard work, ability to get noticed & become a positive presence 
-      in the workplace & ability to learn quickly, identify & solve problems 
-      led me to get promoted as a 'Cook' in just six months. 
-      That is where I gained experience working in a high-speed or fast-paced environment."
-      desc2="I quit the job at the end of '2022' and started working in a pharmacy retail as a 
-      'Retail Assistant' to enhance my communication and customer service skill."
+      title={aboutData[1].title}
+      desc={aboutData[1].desc}
+      desc2={aboutData[1].desc2}
       quote="“I have gone from slicing sashimis to slicing arrays”"
       image={background}
       isPreviewOn={isPreviewOn}
@@ -56,35 +40,16 @@ function AboutMe({ shadow, play, hover }) {
       shadow={shadow}
     />,
     <AboutItem
-      title="Academic Evolution"
-      desc="I began my academic journey with a Bachelor of IT 
-    degree, where I gained a strong foundation in computer 
-    science and software development. However, I
-     realized that I wanted to specialize in software 
-     engineering and decided to take a bootcamp to 
-     further my skills in this field. 
-     The bootcamp provided me with a more hands-on and 
-     practical approach to software engineering, and 
-     I was able to work on real-world projects and learn 
-     from experienced industry professionals."
-      desc2="This experience allowed me to apply the 
-     theoretical knowledge I had learned in my Bachelor's 
-     degree to real-world situations and helped me to become more
-     well-rounded as a software engineer. It also helped 
-     me to become more confident in my abilities and 
-     pursue a career in the field. I am now ready to apply the
-      skills and knowledge I've gained in both 
-      my Bachelor's degree and bootcamp to my future job as 
-      a software engineer. This academic journey has been 
-      a great opportunity for me to develop my skills
-      and pursue my passion for software engineering."
+      title={aboutData[2].title}
+      desc={aboutData[2].desc}
+      desc2={aboutData[2].desc2}
       image={academyXi}
       isPreviewOn={isPreviewOn}
       onPreviewClick={onPreviewClick}
       hover={hover}
       shadow={shadow}
     />,
-    <FromTo edu={edu} exp={exp} shadow={shadow}/>,
+    <FromTo edu={edu} exp={exp} shadow={shadow} />,
   ];
 
   const onNextClick = () => {
@@ -113,7 +78,7 @@ function AboutMe({ shadow, play, hover }) {
         // style={{ boxShadow: `5px -5px 7px ${shadow}` }}
       >
         {pages[pageCount]}
-        <br/>
+        <br />
         <div className="about-buttons">
           <button
             className="nav-button previous"
