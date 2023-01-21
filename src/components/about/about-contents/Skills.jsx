c9583b96e7381f8755d165a6c4b82ae15db6915b
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-22 00:03:10 +1100

    devicon frameworks

--- src/components/about/about-contents/Skills.jsx
+++ src/components/about/about-contents/Skills.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./skills.css";
-import { DiHtml5, DiCss3, DiJavascript1, DiJava } from "react-icons/di";
+import { DiHtml5, DiCss3, DiJavascript1, DiJava, DiReact, DiNodejs, DiRubyRough, DiGrails, DiRuby, DiWordpress } from "react-icons/di";
 
 function Skills({ shadow }) {
   return (
@@ -14,11 +14,24 @@ function Skills({ shadow }) {
       <div className="languages">
         <h5>Languages:</h5>
         <br />
-
+        <div className="icons-container">
           <DiHtml5 style={{ fontSize: "50px" }} />
           <DiCss3 style={{ fontSize: "50px" }} />
           <DiJavascript1 style={{ fontSize: "50px" }} />
+          <DiRuby style={{ fontSize: "50px" }} />
           <DiJava style={{ fontSize: "50px" }} />
+          
+        </div>
+      </div>
+      <br/>
+      <div className="languages">
+        <h5>Frameworks:</h5>
+        <br />
+        <div className="icons-container">
+          <DiReact style={{ fontSize: "50px" }} />
+          <DiNodejs style={{ fontSize: "50px" }} />
+          <DiRubyRough style={{ fontSize: "50px" }} />
+        </div>
       </div>
     </div>
   );
