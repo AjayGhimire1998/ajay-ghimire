f6a5edda5ea0bd28709ea54300c38b1c408b094f
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-22 21:38:56 +1100

    projects wrong

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -4,7 +4,7 @@ import { videoLinks } from "./projects-videos";
 import ProjectItem from "./ProjectItem";
 
 function Projects({ shadow }) {
-  const [pageCount, setPageCount] = useState(0);
+  // const [pageCount, setPageCount] = useState(0);
   const projects = [
     <ProjectItem shadow={shadow} name={videoLinks[0].name} url={videoLinks[0].url} desc={videoLinks[0].desc} desc2={videoLinks[0].desc2} links={videoLinks[0].links} tools={videoLinks[0].tools} />,
   ]
