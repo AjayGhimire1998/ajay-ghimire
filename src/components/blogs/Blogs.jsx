410301bd60273ca7fcb57f7369bf59a77803a8e0
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-23 21:43:33 +1100

    started on blogs

---
+++ src/components/blogs/Blogs.jsx
@@ -0,0 +1,11 @@
+import "./blogs.css";
+
+function Blogs() {
+  return (
+    <div className="blogs-container">
+        Blogsssss....
+    </div>
+  )
+}
+
+export default Blogs
---
+++ src/pages/blog/Blog.jsx
@@ -0,0 +1,13 @@
+import Blogs from "../../components/blogs/Blogs";
+import "./blog.css";
+
+
+function Blog() {
+  return (
+    <div className="blog">
+        <Blogs />
+    </div>
+  )
+}
+
+export default Blog
