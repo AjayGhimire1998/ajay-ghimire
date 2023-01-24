72cd8fbb0626e12b3cc292e3c5200e0011e31b32
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-24 18:32:30 +1100

    Creating each blog item

---
+++ src/components/blogs/BlogItem.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BlogItem() {
+  return (
+    <div>BlogItem</div>
+  )
+}
+
+export default BlogItem
--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -4,11 +4,6 @@ function Blogs() {
   return (
     <div className="blogs-container">
         Blogsssss....
-        afjsgfj
-        afsjhfjkaskjf
-        on mediummmm
-        I have had like 6 already
-        lets fill everything up
     </div>
   )
 }
