908befee26187007bc549ee306a0d9b7759d4071
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-24 20:33:39 +1100

    added contacts svgs and added jumping animation

--- src/App.js
+++ src/App.js
@@ -1,79 +1,101 @@
-import { useEffect, useState, useMemo } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import useSound from 'use-sound';
-import click from './sounds/click.wav';
-import hover from './sounds/hover.mp3';
-import logo1 from './images/logo.png';
-import logo2 from './images/logo2.png';
-import Home from './pages/home/Home';
-import Logo from './components/home/Logo';
-import Navbar from './components/navbar/Navbar';
-import About from './pages/about/About';
-import Project from './pages/project/Project';
-import Resume from './pages/resume/Resume';
-import Blog from './pages/blog/Blog';
-import Contact from './components/contact/Contact';
+import { useEffect, useState, useMemo } from "react";
+import { Routes, Route } from "react-router-dom";
+import useSound from "use-sound";
+import click from "./sounds/click.wav";
+import hover from "./sounds/hover.mp3";
+import logo1 from "./images/logo.png";
+import logo2 from "./images/logo2.png";
+import Home from "./pages/home/Home";
+import Logo from "./components/home/Logo";
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
+import Project from "./pages/project/Project";
+import Resume from "./pages/resume/Resume";
+import Blog from "./pages/blog/Blog";
+import Contact from "./components/contact/Contact";
 
 function App() {
-	const [play] = useSound(click);
-	const [onHover] = useSound(hover);
+  const [play] = useSound(click);
+  const [onHover] = useSound(hover);
 
-	let shadows = useMemo(() => [], []);
-	shadows = ['#80dfff', '#cc6600', '#00ccff', '#cc6600', '#00cc00', '#ffff00', '#ff00ff', '#ff9900'];
-	let logos = useMemo(() => [], []);
-	logos = [logo1, logo2];
+  let shadows = useMemo(() => [], []);
+  shadows = [
+    "#80dfff",
+    "#cc6600",
+    "#00ccff",
+    "#cc6600",
+    "#00cc00",
+    "#ffff00",
+    "#ff00ff",
+    "#ff9900",
+  ];
+  let logos = useMemo(() => [], []);
+  logos = [logo1, logo2];
 
-	const [shadow, setShadow] = useState('');
-	const [logo, setLogo] = useState(logos[0]);
+  const [shadow, setShadow] = useState("");
+  const [logo, setLogo] = useState(logos[0]);
 
-	const changeColors = () => {
-		setTimeout(() => {
-			setShadow(shadows[Math.floor(Math.random() * (7 - 0) + 0)]);
-			changeColors();
-		}, 1000);
-	};
+  const changeColors = () => {
+    setTimeout(() => {
+      setShadow(shadows[Math.floor(Math.random() * (7 - 0) + 0)]);
+      changeColors();
+    }, 1000);
+  };
 
-	const changeLogo = () => {
-		setTimeout(() => {
-			setLogo(logos[Math.floor(Math.random() * (2 - 0) + 0)]);
-			changeLogo();
-		}, 1000);
-	};
+  const changeLogo = () => {
+    setTimeout(() => {
+      setLogo(logos[Math.floor(Math.random() * (2 - 0) + 0)]);
+      changeLogo();
+    }, 1000);
+  };
 
-	useEffect(() => {
-		changeColors();
-		changeLogo();
-		// window.onbeforeunload = () => {
-		// 	localStorage.clear();
-		// };
-	}, []);
+  useEffect(() => {
+    changeColors();
+    changeLogo();
+    // window.onbeforeunload = () => {
+    // 	localStorage.clear();
+    // };
+  }, []);
 
-	return (
-		<>
-			<div className="space"></div>
-			<div className="main">
-				<Logo logo={logo} shadow={shadow} />
-				<Routes>
-					<Route path="/" element={<Home shadow={shadow} />} />
-					<Route path="/about" element={<About shadow={shadow} play={play} hover={onHover} />} />
-					<Route path="/projects" element={<Project shadow={shadow} play={play} hover={onHover} />} />
-					<Route path="/blogs" element={<Blog shadow={shadow} play={play} hover={onHover} />} />
-					<Route path="/resume" element={<Resume shadow={shadow} play={play} hover={onHover} />} />
-				</Routes>
-				<br />
-				<br />
-				<Navbar play={play} hover={onHover} />
-				<br />
-				<br />
-				<br />
-				<Contact />
-
-				<footer className="footer">
-					<p style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>© 2023 Ajay Ghimire</p>
-				</footer>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="space"></div>
+      <div className="main">
+        <Logo logo={logo} shadow={shadow} />
+        <Routes>
+          <Route path="/" element={<Home shadow={shadow} />} />
+          <Route
+            path="/about"
+            element={<About shadow={shadow} play={play} hover={onHover} />}
+          />
+          <Route
+            path="/projects"
+            element={<Project shadow={shadow} play={play} hover={onHover} />}
+          />
+          <Route
+            path="/blogs"
+            element={<Blog shadow={shadow} play={play} hover={onHover} />}
+          />
+          <Route
+            path="/resume"
+            element={<Resume shadow={shadow} play={play} hover={onHover} />}
+          />
+        </Routes>
+        <br />
+        <br />
+        <Navbar play={play} hover={onHover} />
+        <br />
+        <br />
+        <br />
+        <Contact />
+        <footer className="footer">
+          <p style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>
+            © 2023 Ajay Ghimire
+          </p>
+        </footer>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,11 +4,11 @@ import "./contact.css"
 function Contact() {
     return (
         <section className="contact-container">
-            <LinkedInSvg />
-            <GithubSvg />
-            <EmailSvg />
-            <FacebookSvg />
-            <InstaSvg />
+            <span className="kreep"><LinkedInSvg className=" linkedin" /></span>
+            <span className="kreep"><GithubSvg className=" github" /></span>
+            <span className="kreep"><EmailSvg className=" email" /></span>
+            <span className="kreep"><FacebookSvg className=" facebook" /></span>
+            <span className="kreep"><InstaSvg className=" insta" /></span>
         </section>
     )
 }
