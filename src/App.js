79ab3896207752e6ff0d6c97d94460d833334679
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-25 22:16:46 +1100

    fixed state persistence bugs

--- src/App.js
+++ src/App.js
@@ -45,10 +45,13 @@ function App() {
 	useEffect(() => {
 		changeColors();
 		changeLogo();
-		if (localStorage !== null) {
+		if (localStorage.getItem("current_page")) {
       navigate(`${localStorage.getItem("current_page").slice(1, -1).toLocaleLowerCase()}`)
-		}
+		} else {
+      navigate("")
+    }
 	}, []);
+
 
 	return (
 		<>
