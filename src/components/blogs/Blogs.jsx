f3afe3f49d45e373bb0084817517ee71e95b10fa
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-25 21:32:50 +1100

    fixed nav bugs

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -2,15 +2,16 @@ import BlogItem from './BlogItem';
 import './blogs.css';
 import { blogsData } from './blogs-data';
 import { useState } from 'react';
+import {v4 as uuidv4} from "uuid"
 
 function Blogs() {
 	const [blogCount, setBlogCount] = useState(4);
 	const blogItem = blogsData.map((blog) => {
-		return <BlogItem blog={blog} />;
+		return <BlogItem blog={blog} key={uuidv4()}/>;
 	});
 
 	const onSeeMore = () => {
-		setBlogCount(blogItem.length);
+		setBlogCount(blogCount + 4);
 	};
 
 	const onSeeLess = () => {
