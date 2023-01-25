c2c97cda4e0eb4858d9a7c6fa39049fd568c3e39
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-25 19:46:55 +1100

    adding see more

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,17 +1,33 @@
-import BlogItem from "./BlogItem";
-import "./blogs.css";
-import { blogsData } from "./blogs-data";
+import BlogItem from './BlogItem';
+import './blogs.css';
+import { blogsData } from './blogs-data';
+import { useState } from 'react';
 
 function Blogs() {
+	const [blogCount, setBlogCount] = useState(4);
+	const blogItem = blogsData.map((blog) => {
+		return <BlogItem blog={blog} />;
+	});
 
-  const blogItem = blogsData.map((blog) => {
-    return <BlogItem blog={blog}/>
-  })
-  return (
-    <div className="blogs-container">
-        {blogItem}
-    </div>
-  )
+	const onSeeMore = () => {
+		setBlogCount(blogItem.length);
+	};
+	return (
+		<div className="blogs-container">
+			{blogItem.slice(0, blogCount)}
+			<p
+				onClick={
+					blogCount === 4
+						? onSeeMore
+						: () => {
+								setBlogCount(4);
+						  }
+				}
+			>
+				See More
+			</p>
+		</div>
+	);
 }
 
-export default Blogs
+export default Blogs;
