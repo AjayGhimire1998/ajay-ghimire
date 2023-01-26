10d21ac42890c01bc716281e49b56a4d46c0b895
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-26 21:40:37 +1100

    adding songs

--- src/components/cheer/songs.js
+++ src/components/cheer/songs.js
@@ -1,1 +1,8 @@
-export const songsUrl = ['https://youtu.be/dQw4w9WgXcQ', 'https://youtu.be/4fndeDfaWCg'];
+export const songsUrl = [
+	'https://youtu.be/dQw4w9WgXcQ',
+	'https://youtu.be/4fndeDfaWCg',
+	'https://youtu.be/PpsUOOfb-vE',
+	'https://youtu.be/e1FN047_LT0',
+    "https://youtu.be/l_IsV3IjoBM",
+    "https://youtu.be/851e5KqXrpA"
+];
