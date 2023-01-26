7b34ba32f613b6c8fb95d9cbbb94ad426321ed21
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-26 21:43:59 +1100

    react player

---
+++ src/components/cheer/CheerPlayer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CheerPlayer() {
+  return (
+    <div>CheerPlayer</div>
+  )
+}
+
+export default CheerPlayer
--- src/components/cheer/songs.js
+++ src/components/cheer/songs.js
@@ -4,5 +4,8 @@ export const songsUrl = [
 	'https://youtu.be/PpsUOOfb-vE',
 	'https://youtu.be/e1FN047_LT0',
     "https://youtu.be/l_IsV3IjoBM",
-    "https://youtu.be/851e5KqXrpA"
+    "https://youtu.be/851e5KqXrpA",
+    "https://youtu.be/BN1WwnEDWAM",
+    "https://youtu.be/ABfQuZqq8wg",
+    "https://youtu.be/-eHJ12Vhpyc"
 ];
