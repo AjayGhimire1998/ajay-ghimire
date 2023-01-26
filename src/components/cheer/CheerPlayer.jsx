b77263a2ac36d847aee6d4e347ab786f370337cb
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-26 22:08:48 +1100

    react player

--- src/components/cheer/CheerPlayer.jsx
+++ src/components/cheer/CheerPlayer.jsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
+import React, {useEffect} from 'react';
+import YouTube from "react-youtube";
 import { songsUrl } from './songs';
 
 function CheerPlayer() {
-	return (
-		<div style={{display: "none"}}>
-			<ReactPlayer playing url={songsUrl} volume={0.2} />
-		</div>
+
+    const opts = {
+        height: '0',
+        width: '0',
+        playerVars: {
+          autoplay: 1,
+          controls: 1,
+        }
+      };
+      const playerRef = React.createRef();
+      useEffect(() => {
+        playerRef.current.internalPlayer.playVideo();
+      }, [])
+      return (
+        <YouTube 
+          videoId={songsUrl[0]}
+          opts={opts}
+          ref={playerRef}
+        />
 	);
 }
 
