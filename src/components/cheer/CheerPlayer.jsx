2ceed74f6e2ebe7d5e3d7dee5afc30af287d192f
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-30 22:04:36 +1100

    working on music

--- src/components/cheer/CheerPlayer.jsx
+++ src/components/cheer/CheerPlayer.jsx
@@ -1,45 +1,68 @@
 import React, { useState } from 'react';
+import { useRef } from 'react';
 import { useEffect } from 'react';
-// import AudioPlayer from 'react-audio-player';
+
 import ReactAudioPlayer from 'react-player';
 import { songsUrl } from './songs';
 
 function CheerPlayer() {
 	const [isPlaying, setIsPlaying] = useState(false);
-	const [song, setSong] = useState(new Audio(songsUrl[Math.floor(Math.random() * 9)]));
+	const [trackIndex, setTrackIndex] = useState(Math.floor(Math.random() * 9));
 
-	const nextClick = () => {
-		if (isPlaying) {
-			setSong(null);
-		}
-		setSong(new Audio(songsUrl[Math.floor(Math.random() * 9)]));
-		onPlay(song);
+	const songRef = useRef(new Audio(songsUrl[trackIndex]));
+	const intervalRef = useRef();
+	const isReady = useRef(false);
+
+	const onNextClick = () => {
+		setTrackIndex(Math.floor(Math.random() * 9));
 	};
-	const onPlay = (music) => {
-		music.play();
-		setIsPlaying(true);
-	};
-	const onPause = (music) => {
-		music.pause();
-		setIsPlaying(false);
+
+	const onPlayPauseClick = (value) => {
+		setIsPlaying(value);
 	};
 
 	useEffect(() => {
-		setSong(song)
-	}, [song, nextClick]);
+		if (isPlaying) {
+			songRef.current.play();
+		} else {
+			songRef.current.pause();
+		}
+	}, [isPlaying]);
+
+	useEffect(() => {
+		songRef.current.pause();
+		clearInterval(intervalRef.current);
+	}, []);
+
+	useEffect(() => {
+		songRef.current.pause();
+		songRef.current = new Audio(songsUrl[trackIndex]);
+
+		if (isReady.current) {
+			songRef.current.play();
+			setIsPlaying(true);
+		} else {
+			isReady.current = true;
+		}
+	}, [trackIndex]);
 	return (
 		<>
-			{/* <ReactAudioPlayer url={} volume={0.05} playing={true} controls/> */}
 			{isPlaying ? (
-				<button style={{ display: 'inline-block', width: '50%', color: 'black' }} onClick={() => onPause(song)}>
+				<button
+					style={{ display: 'inline-block', width: '50%', color: 'black' }}
+					onClick={() => onPlayPauseClick(false)}
+				>
 					Pause
 				</button>
 			) : (
-				<button style={{ display: 'inline-block', width: '50%', color: 'black' }} onClick={() => onPlay(song)}>
+				<button
+					style={{ display: 'inline-block', width: '50%', color: 'black' }}
+					onClick={() => onPlayPauseClick(true)}
+				>
 					Play
 				</button>
 			)}
-			<button onClick={nextClick} style={{ display: 'inline-block', width: '50%', color: 'black' }}>
+			<button onClick={onNextClick} style={{ display: 'inline-block', width: '50%', color: 'black' }}>
 				Next
 			</button>
 		</>
