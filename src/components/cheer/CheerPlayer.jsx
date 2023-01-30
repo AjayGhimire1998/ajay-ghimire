622c7d74397f5a00d6ba11a31b11e30eee4d37b8
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-30 21:22:14 +1100

    working on music

--- src/components/cheer/CheerPlayer.jsx
+++ src/components/cheer/CheerPlayer.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 // import AudioPlayer from 'react-audio-player';
 import ReactAudioPlayer from 'react-player';
 import { songsUrl } from './songs';
@@ -7,9 +8,9 @@ function CheerPlayer() {
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [song, setSong] = useState(new Audio(songsUrl[0]));
 
-
 	const nextClick = () => {
 		setSong(new Audio(songsUrl[Math.floor(Math.random() * 9)]));
+		song.play()
 	};
 	const onPlayPauseClick = () => {
 		if (isPlaying) {
@@ -19,13 +20,19 @@ function CheerPlayer() {
 		}
 		setIsPlaying(!isPlaying);
 	};
+
+	useEffect(() => {
+		setIsPlaying(false)
+	}, [song])
 	return (
 		<>
 			{/* <ReactAudioPlayer url={} volume={0.05} playing={true} controls/> */}
 			<button style={{ display: 'inline-block', width: '50%', color: 'black' }} onClick={onPlayPauseClick}>
 				{isPlaying ? 'Pause' : 'Cheer Me Up'}
 			</button>
-			<button onClick={nextClick} >Next</button>
+			<button onClick={nextClick} style={{ display: 'inline-block', width: '50%', color: 'black' }}>
+				Next
+			</button>
 		</>
 	);
 }
