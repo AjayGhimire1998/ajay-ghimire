38eac5525e292c920670c008829c5537a17f53c1
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-30 19:33:04 +1100

    profile card

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -3,27 +3,25 @@ import './intro.css';
 import ajay from '../../images/my-pictures/ajay1.jpeg';
 function Intro({ shadow }) {
 	return (
-		<>
-			<div className="avatar" >
+		<div className="intro-container">
+			<div className="avatar" style={{ backgroundImage: `url(${ajay})` }}>
 				<img src={ajay} alt="avatar" className="avatar-image" />
 			</div>
-			<div className="intro-container">
-				<br />
-				<p>I am</p>
-				<br />
-				<div style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>
-					<strong>Ajay Ghimire</strong>
-				</div>
-				<br />
-				<p>a </p>
-				<span>Full Stack Software Developer </span>
-				<p> from </p>
-				<span>
-					<i> Sydney, Australia.</i>
-				</span>
-				<br />
+			{/* <br /> */}
+			<p>I am</p>
+			<br />
+			<div style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>
+				<strong>Ajay Ghimire</strong>
 			</div>
-		</>
+			<br />
+			<p>a </p>
+			<span>Full Stack Software Developer </span>
+			<p> from </p>
+			<span>
+				<i> Sydney, Australia.</i>
+			</span>
+			<br />
+		</div>
 	);
 }
 
