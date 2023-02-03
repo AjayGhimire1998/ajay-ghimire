32eef8e53837ebe81bddc783c8237a44b62e6fab
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-03 20:38:43 +1100

    almost done

--- src/components/about/about-contents/Skills.jsx
+++ src/components/about/about-contents/Skills.jsx
@@ -1,52 +1,61 @@
-import React from "react";
-import "./skills.css";
-import {CSSSvg, HTMLSvg, JAVASvg, JSSvg, MONGOSvg, MYSQLSvg, NODESvg, POSTGRESSvg, RAILSSvg, REACTSvg, RUBYSvg} from "./devIcons.js";
+import React from 'react';
+import './skills.css';
+import {
+	CSSSvg,
+	HTMLSvg,
+	JAVASvg,
+	JSSvg,
+	MONGOSvg,
+	MYSQLSvg,
+	NODESvg,
+	POSTGRESSvg,
+	RAILSSvg,
+	REACTSvg,
+	RUBYSvg,
+} from './devIcons.js';
 
 function Skills({ shadow }) {
-  return (
-    <div className="skills-container">
-      <h3 style={{ textAlign: "center" }}>
-        <span style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>
-          Technical Skills
-        </span>
-      </h3>
-      <br />
-      <div className="languages">
-        <h5 >Languages, I can code on:</h5>
-        <br />
-        <div className="icons-container">
-          <HTMLSvg />
-          <CSSSvg />
-          <JSSvg />
-          <RUBYSvg/>
-          <JAVASvg/>
-          
-        </div>
-      </div>
-      <br/>
-      <br/>
-      <div className="languages">
-        <h5>Frameworks, I am familiar with:</h5>
-        <br />
-        <div className="icons-container">
-          <REACTSvg />
-          <NODESvg />
-          <RAILSSvg />
-        </div>
-      </div>
-      <br/>
-      <br/>
-      <div className="languages">
-        <h5>Database, I use:</h5>
-        <br />
-        <div className="icons-container">
-          <MYSQLSvg/>
-          <MONGOSvg />
-          <POSTGRESSvg />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="skills-container">
+			<h3 style={{ textAlign: 'center' }}>
+				<span style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>Technical Skills</span>
+			</h3>
+			<br />
+			<div className="languages">
+				<h6>Languages, I can code on:</h6>
+				<br />
+				<div className="icons-container">
+					<HTMLSvg />
+					<CSSSvg />
+					<JSSvg />
+					<RUBYSvg />
+					<JAVASvg />
+				</div>
+			</div>
+			<br />
+			<br />
+			<div className="languages">
+				<h6>Frameworks, I am familiar with:</h6>
+				<br />
+				<div className="icons-container">
+					<REACTSvg />
+					<NODESvg />
+					<RAILSSvg />
+				</div>
+			</div>
+			<br />
+			<br />
+			<div className="languages">
+				<h6>Database, I use:</h6>
+				<br />
+				<div className="icons-container">
+					<MYSQLSvg />
+					<MONGOSvg />
+					<POSTGRESSvg />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Skills;
