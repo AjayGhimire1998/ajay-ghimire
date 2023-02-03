ccd166900537edf3ee0592a9768550350b191a37
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-03 19:16:51 +1100

    making profile better

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './intro.css';
 import ajay from '../../images/my-pictures/ajay1.jpeg';
-import cover from '../../images/background.png';
+import cover from '../../images/cover.png';
 import pin from '../../images/my-pictures/pin.png';
 import { useNavigate } from 'react-router-dom';
 function Intro({ shadow, play}) {
