bcd34c0268bd24468316066a46f835e8f61c3111
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-04 20:38:47 +1100

    almost done

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './intro.css';
 import ajay from '../../images/my-pictures/ajay1.jpeg';
-import cover from '../../images/cover.jpeg';
+import cover from '../../images/coverr.png';
 import pin from '../../images/my-pictures/pin.png';
 import { useNavigate } from 'react-router-dom';
 function Intro({ shadow, play}) {
