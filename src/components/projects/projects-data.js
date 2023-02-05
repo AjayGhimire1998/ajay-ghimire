0ac8f0a68a77c6dc8fa5f2405d16cce529d2000c
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-05 21:00:50 +1100

    fixing small stuffs

--- src/components/projects/projects-data.js
+++ src/components/projects/projects-data.js
@@ -16,7 +16,7 @@ export const videoLinks = [
 	{
 		name: 'WithLoveFromHome',
 		url: 'https://youtu.be/IEl0hS8OP2k',
-		desc: 'A full-stack E-commerce web application built using React for the front end and Rails to develop the server.',
+		desc: 'A full-stack E-commerce web application developed using React for UI and Rails for server.',
 		desc2: 'It allows users to register as a store who can create products to sell and also allows users to register as a customer who can choose through the products to add to cart and checkout. Customers can leave reviews to the products and the stores. The store and customer get notified when an order is made or its status has changed through email.',
 		links: {
 			git: 'https://github.com/AjayGhimire1998/with-love-from-home',
@@ -30,7 +30,7 @@ export const videoLinks = [
 	{
 		name: 'GoalsToScore',
 		url: 'https://youtu.be/aAT7HKS-DdU',
-		desc: 'A simple goals management MERN stack app to create and update goals and their sub-tasks.',
+		desc: 'A MERN stack app for effective goals management, allowing for creation and updates of goals and sub-tasks.',
 		desc2: 'The app requires a simple authentication, and allows a user to save their goals with tasks related to them. They can later add more tasks as needed and switch the status of each task to either pending or completed which updates in the backend synchronously.',
 		links: {
 			live: 'https://ajayghimire1998.github.io/GoalsToScore/',
@@ -45,7 +45,7 @@ export const videoLinks = [
 	{
 		name: 'MemeBook',
 		url: 'https://youtu.be/JTJ3IyiqIqk',
-		desc: 'A React app with user authentication, for viewing, uploading, and creating memes, built using Jokes Rapid API, Reddit API and IMGFlip API and Firebase to handle Authentication and storage.',
+		desc: 'A React app with user authentication, allowing users to view, upload, and create memes using multiple APIs and Firebase for authentication and storage.',
 		desc2: 'Users can register and login using their email and view the memes in the news feed, set up their profile and be able to upload memes of their own and also create memes with the help of templates generated from FLIPImg API and adding texts of their own or automated jokes from Rapid/JOKES API and also download the newly created meme to their local device and upload it back to the feed for others to see.',
 		links: {
 			live: 'https://ajayghimire1998.github.io/memebook/',
@@ -56,7 +56,7 @@ export const videoLinks = [
 	{
 		name: 'CryptoMania',
 		url: 'https://youtu.be/EYW5ZX8XiWk',
-		desc: 'A Ruby CLI gem which runs on Terminal, a game where a player can invest funds provided in cryptocurrencies in the past and check on the future, if the player has successfully doubled the investment to win or not.',
+		desc: 'A Ruby CLI gem game that simulates investment in historical cryptocurrencies with the aim to double funds and win the game, which runs on Terminal',
 		desc2: "It allows players to enter the past dates and invest given initial balance into their favourite crypto currencies, read about the currencies, and checkout in any future date they like to, and the program calculates the total earning at last to finalize a winner. It uses 'Nokogiri' to scrape details about the crypto and a crypto API to fetch rates data.",
 		links: {
 			live: 'https://rubygems.org/search?query=cryptomania',
@@ -67,7 +67,7 @@ export const videoLinks = [
 	{
 		name: 'PlayMates',
 		url: 'https://youtu.be/HV8v0qiuvtI',
-		desc: 'A Ruby on Rails app with user authentication that allows users to view and upload gaming contents like images and videos as posts and live chat with other users to team up to play video games of their similar choices.',
+		desc: 'A Ruby on Rails gaming platform with user authentication, enabling users to post, share and chat about gaming contents, facilitating team-ups for like-minded players',
 		desc2: 'The users can register and login using their email and view the gaming contents posted in the feed, set up their profile and be able to contents of their own, create a category their posts would belong to, and use it to filter the relative contents on that category. They can also team up with others directly through a chat system.',
 		links: {
 			git: 'https://github.com/AjayGhimire1998/Play-Mates',
