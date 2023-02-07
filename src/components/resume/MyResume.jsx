4e15cfe55578351dfa332cb93d0205ba1f3c3f44
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-07 23:33:25 +1100

    making dynamic routes for each projects

--- src/components/resume/MyResume.jsx
+++ src/components/resume/MyResume.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import myResume from '../../images/Resume_Ajay_Ghimire.pdf';
+import myResume from '../../images/Ajay_Ghimire_Resume.pdf';
 import './my-resume.css';
 
 function MyResume({ play, hover }) {
 	return (
 		<div className="resume-container">
 			<div className="my-resume-container">
-				<iframe src={myResume + '#toolbar=0'} title="Resume_Ajay_Ghimire" className="my-resume" />
+				<iframe src={myResume + '#toolbar=0'} title="Ajay_Ghimire_Resume" className="my-resume" />
 			</div>
 			<div className="download-button-container">
 				<a
@@ -14,7 +14,7 @@ function MyResume({ play, hover }) {
 					className="nav-button download"
 					onClick={play}
 					onMouseOver={hover}
-					download="Resume_Ajay_Ghimire.pdf"
+					download="Ajay_Ghimire_Resume.pdf"
 				>
 					Download
 				</a>
