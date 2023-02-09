9096bb3cd1063ee35f08c2d747c5a1f58bf17034
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-09 20:47:28 +1100

    adding titles

--- src/components/about/about-contents/Skills.jsx
+++ src/components/about/about-contents/Skills.jsx
@@ -20,9 +20,14 @@ import {
 	SVELTESvg,
 	VSCODESvg,
 } from './devIcons.js';
-import { ToolTips } from './ToolTip';
+import { Tooltip } from 'react-tooltip';
 
 function Skills({ shadow }) {
+	const toolTipStyle = {
+		height: '10px',
+		width: 'auto',
+		fontSize: '10px',
+	};
 	return (
 		<div className="skills-container">
 			<h3 style={{ textAlign: 'center' }}>
@@ -32,14 +37,30 @@ function Skills({ shadow }) {
 			<div className="languages">
 				<h6>Languages:</h6>
 				<div className="icons-container">
-					<HTMLSvg onMouseOver={() =><ToolTips name="HTML" />} />
-					<CSSSvg />
-					<JSSvg />
-					<RUBYSvg />
-					<JAVASvg />
+					<div id="html" data-tooltip-content="HTML" data-tooltip-place="bottom">
+						<HTMLSvg />
+						<Tooltip anchorId="html" style={toolTipStyle} />
+					</div>
+					<div id="css" data-tooltip-content="CSS" data-tooltip-place="bottom">
+						<CSSSvg />
+						<Tooltip anchorId="css" style={toolTipStyle} />
+					</div>
+					<div id="JS" data-tooltip-content="JavaScript" data-tooltip-place="bottom">
+						<JSSvg />
+						<Tooltip anchorId="JS" style={toolTipStyle} />
+					</div>
+					<div id="ruby" data-tooltip-content="RUBY" data-tooltip-place="bottom">
+						<RUBYSvg />
+						<Tooltip anchorId="ruby" style={toolTipStyle} />
+					</div>
+					<div id="css" data-tooltip-content="CSS" data-tooltip-place="bottom">
+						<JAVASvg />
+						<Tooltip anchorId="css" style={toolTipStyle} />
+					</div>
 				</div>
 			</div>
 			<br />
+
 			<div className="languages">
 				<h6>Frameworks:</h6>
 				<div className="icons-container">
@@ -62,7 +83,8 @@ function Skills({ shadow }) {
 			<br />
 			<div className="languages">
 				<h6>Tools:</h6>
-				<div className="icons-container">~
+				<div className="icons-container">
+					~
 					<VSCODESvg />
 					<GITSvg />
 					<POSTMANSvg />
