6e158c46f1b38a97f8c3682a139021c9ba771b65
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-11 22:41:39 +1100

    lazy loading

--- src/components/home/Logo.jsx
+++ src/components/home/Logo.jsx
@@ -1,8 +1,8 @@
-
+import { useNavigate } from "react-router-dom";
 import "./logo.css";
 
 function Logo({logo, shadow}) {
-  
+  const navigate = useNavigate();
   return (
     <div className="logo-container">
       <img
@@ -10,6 +10,7 @@ function Logo({logo, shadow}) {
         alt="logo"
         className="logo"
         style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}
+        onClick={() => navigate('/')}
       />
     </div>
   );
