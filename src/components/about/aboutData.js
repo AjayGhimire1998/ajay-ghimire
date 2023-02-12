a62b360ba97afd4de2024f15a4db71eab0d3a13e
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-12 22:36:48 +1100

    picture slide bugs

--- src/components/about/aboutData.js
+++ src/components/about/aboutData.js
@@ -1,24 +1,26 @@
-import profile from '../../images/my-pictures/profile.jpeg';
+import profile from '../../images/my-pictures/Ajay.png';
 import ajay1 from '../../images/my-pictures/ajay1.jpeg';
-import ajay2 from '../../images/my-pictures/ajay2.jpeg';
-import background from '../../images/my-pictures/chocho.JPG';
+import ajay2 from '../../images/my-pictures/ajay2.png';
+import chocho from '../../images/my-pictures/chocho.JPG';
+import wollies from "../../images/my-pictures/wollies.jpeg"
 import academyXiCert from '../../images/my-pictures/academyXi.png';
 import reactReduxCert from '../../images/my-pictures/react-redux-cert.png';
 import rubyCert from '../../images/my-pictures/ruby-cert.png';
 import responsiveCert from '../../images/my-pictures/responsive-cert.jpg';
+
 
 export const aboutData = [
 	{
 		title: 'Profile',
 		desc: "As a highly motivated and results-driven software developer with a passion for innovation and a strong background in IT, I am eager to bring my skills and expertise to a challenging software developer role. I have recently graduated with a Bachelor's degree in IT and completed a software engineering bootcamp, demonstrating my ability to quickly learn and master new technologies, as well as my track record of delivering high-quality web applications on time.",
 		desc2: "My problem-solving skills are top-notch, and I thrive in a team environment, collaborating effectively with others to deliver exceptional results. I am always seeking opportunities to challenge myself and grow as a professional, which is why I am actively learning the ins and outs of Java (I hear it's like a never-ending adventure!). If you're looking for a software developer who brings a positive, results-driven approach and a bit of humor to the table, look no further. I am ready to take on your toughest projects and help drive your company's success to new heights.",
-		images: [ajay1, ajay2, profile],
+		images: [ajay2, ajay1,  profile],
 	},
 	{
 		title: 'Professional Evolution',
 		desc: "During my pursuit of a Bachelor's degree in IT, I had the opportunity to work as a kitchen hand at a bustling Japanese restaurant. My dedication to hard work and quick learning abilities, combined with my positive attitude and problem-solving skills, earned me a promotion to Cook within just six months. This experience honed my ability to work effectively under pressure and in fast-paced environments.",
 		desc2: "After leaving the restaurant at the end of 2022, I embarked on a new journey as a Retail Assistant in a pharmacy setting. This role has allowed me to further develop my communication and customer service skills, which are crucial in any industry. In my free time, I like to joke that I'm also a master at counting pills and sorting medication – though I'll leave the actual prescriptions to the pharmacists.",
-		images: [background],
+		images: [chocho, wollies],
 	},
 	{
 		title: 'Academic Evolution',
