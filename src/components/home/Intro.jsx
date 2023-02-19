a2123ec708192dc46c8c9c0216292af724f594d7
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-19 21:17:55 +1100

    toggled on hover

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,54 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactPlayer from "react-player";
 import { useNavigate } from "react-router-dom";
 import "./intro.css";
 import ajay from "../../images/my-pictures/profile_pic.png";
 import cover from "../../images/cover.png";
 import pin from "../../images/my-pictures/pin.png";
-import ppvideo from "../../videos/PortfolioVideo.mp4"
+import ppvideo from "../../videos/PortfolioVideo.mp4";
 
 function Intro({ play }) {
   const navigate = useNavigate();
-//   const [profileHover, set]
+  const [profileHover, setProfileHover] = useState(false);
   function goToAbout() {
     play();
     navigate("/about");
   }
 
-  function playProfileVideo() {
-    return (
-      <ReactPlayer
-        // url="../../videos/PortfolioVideo.mp4"
-        width="100%"
-        height="100%"
-        controls={true}
-      />
-    );
-  }
-
   return (
-    <div className="intro-container">
-      <div
-        className="avatar"
-        style={{
-          backgroundImage: `url(${cover})`,
-          backgroundSize: "100% 100%",
-        }}
-      >
-        <img
-          src={ajay}
-          alt="avatar"
-          className="avatar-image"
-          onClick={goToAbout}
-    
+    <div
+      className="intro-container"
+      onMouseLeave={() => setProfileHover(false)}
+    >
+      {profileHover ? (
+        <ReactPlayer
+          url={ppvideo}
+          width="100%"
+          height="100%"
+          controls={false}
         />
-      </div>
-	  <ReactPlayer
-        url={ppvideo}
-        width="100%"
-        height="100%"
-        controls={true}
-      />
+      ) : (
+        <div
+          className="avatar"
+          style={{
+            backgroundImage: `url(${cover})`,
+            backgroundSize: "100% 100%",
+          }}
+        >
+          <img
+            src={ajay}
+            alt="avatar"
+            className="avatar-image"
+            onClick={goToAbout}
+            onMouseEnter={() => setProfileHover(true)}
+          />
+        </div>
+      )}
+
       <div className="contents">
         <p>I am</p>
         <div>
