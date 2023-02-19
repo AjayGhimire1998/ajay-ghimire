f7d89f58075c947d3a86410eac82108d419c4f1e
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-19 21:37:29 +1100

    border filtered

--- src/components/home/Intro.jsx
+++ src/components/home/Intro.jsx
@@ -1,19 +1,13 @@
 import React, { useState } from "react";
 import ReactPlayer from "react-player";
-import { useNavigate } from "react-router-dom";
 import "./intro.css";
 import ajay from "../../images/my-pictures/profile_pic.png";
 import cover from "../../images/cover.png";
 import pin from "../../images/my-pictures/pin.png";
 import ppvideo from "../../videos/PortfolioVideo.mp4";
 
-function Intro({ play, shadow }) {
-  const navigate = useNavigate();
+function Intro({ shadow }) {
   const [profileHover, setProfileHover] = useState(false);
-  function goToAbout() {
-    play();
-    navigate("/about");
-  }
 
   return (
     <div
@@ -40,7 +34,6 @@ function Intro({ play, shadow }) {
             src={ajay}
             alt="avatar"
             className="avatar-image"
-            onClick={goToAbout}
             onMouseEnter={() => setProfileHover(true)}
 			style={{ filter: `drop-shadow(0px 0px 10px ${shadow})`, border: `2px solid ${shadow}` }}
           />
