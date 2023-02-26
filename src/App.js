0f3c54099ece8c120c99c0ce2716f902da403d90
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-26 14:23:16 +1100

    wokring on cheer up button

--- src/App.js
+++ src/App.js
@@ -1,86 +1,107 @@
-import { useEffect, useState, useMemo } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import useSound from 'use-sound';
-import click from './sounds/click.wav';
-import hover from './sounds/hover.mp3';
-import logo1 from './images/logo.png';
-import logo2 from './images/logo2.png';
-import Home from './pages/home/Home';
-import Logo from './components/home/Logo';
-import Navbar from './components/navbar/Navbar';
-import About from './pages/about/About';
-import Project from './pages/project/Project';
-import Resume from './pages/resume/Resume';
-import Blog from './pages/blog/Blog';
-import Contact from './components/contact/Contact';
-import CheerPlayer from './components/cheer/CheerPlayer';
-import NoMatch from './components/NoMatch';
-import EachProject from './pages/project/EachProject';
+import { useEffect, useState, useMemo } from "react";
+import { Routes, Route } from "react-router-dom";
+import useSound from "use-sound";
+import click from "./sounds/click.wav";
+import hover from "./sounds/hover.mp3";
+import logo1 from "./images/logo.png";
+import logo2 from "./images/logo2.png";
+import Home from "./pages/home/Home";
+import Logo from "./components/home/Logo";
+import Navbar from "./components/navbar/Navbar";
+import About from "./pages/about/About";
+import Project from "./pages/project/Project";
+import Resume from "./pages/resume/Resume";
+import Blog from "./pages/blog/Blog";
+import Contact from "./components/contact/Contact";
+import CheerPlayer from "./components/cheer/CheerPlayer";
+import NoMatch from "./components/NoMatch";
+import EachProject from "./pages/project/EachProject";
 
 function App() {
-	const [play] = useSound(click, { volume: 0.1 });
-	const [onHover] = useSound(hover, { volume: 0.1 });
+  const [play] = useSound(click, { volume: 0.1 });
+  const [onHover] = useSound(hover, { volume: 0.1 });
 
-	let shadows = useMemo(() => [], []);
-	shadows = ['#80dfff', '#cc6600', '#00ccff', '#cc6600', '#00cc00', '#ffff00', '#ff00ff', '#ff9900'];
-	let logos = useMemo(() => [], []);
-	logos = [logo1, logo2];
+  let shadows = useMemo(() => [], []);
+  shadows = [
+    "#80dfff",
+    "#cc6600",
+    "#00ccff",
+    "#cc6600",
+    "#00cc00",
+    "#ffff00",
+    "#ff00ff",
+    "#ff9900",
+  ];
+  let logos = useMemo(() => [], []);
+  logos = [logo1, logo2];
 
-	const [shadow, setShadow] = useState('');
-	const [logo, setLogo] = useState(logos[0]);
+  const [shadow, setShadow] = useState("");
+  const [logo, setLogo] = useState(logos[0]);
 
-	const changeColors = () => {
-		setTimeout(() => {
-			setShadow(shadows[Math.floor(Math.random() * (7 - 0) + 0)]);
-			changeColors();
-		}, 1000);
-	};
+  const changeColors = () => {
+    setTimeout(() => {
+      setShadow(shadows[Math.floor(Math.random() * (7 - 0) + 0)]);
+      changeColors();
+    }, 1000);
+  };
 
-	const changeLogo = () => {
-		setTimeout(() => {
-			setLogo(logos[Math.floor(Math.random() * (2 - 0) + 0)]);
-			changeLogo();
-		}, 1000);
-	};
+  const changeLogo = () => {
+    setTimeout(() => {
+      setLogo(logos[Math.floor(Math.random() * (2 - 0) + 0)]);
+      changeLogo();
+    }, 1000);
+  };
 
-	useEffect(() => {
-		changeColors();
-		changeLogo();
-	}, []);
+  useEffect(() => {
+    changeColors();
+    changeLogo();
+  }, []);
 
-	return (
-		<>
-			<div className="space"></div>
-			<div className="main">
-				<Logo logo={logo} shadow={shadow} play={play} />
+  return (
+    <>
+      <div className="space"></div>
+      <div className="main">
+        <Logo logo={logo} shadow={shadow} play={play} />
 
-				<Routes>
-					<Route path="/" element={<Home shadow={shadow} play={play} />} />
-					<Route path="/about" element={<About play={play} hover={onHover} />} />
-					<Route path="/projects" element={<Project  play={play} hover={onHover} />} />
-					<Route
-						path="/projects/:name"
-						element={<EachProject play={play} hover={onHover} />}
-					/>
-					<Route path="/blogs" element={<Blog />} />
-					<Route path="/resume" element={<Resume play={play} hover={onHover} />} />
-					<Route path="*" element={<NoMatch />} />
-				</Routes>
-				<br />
-				<Navbar play={play} hover={onHover} />
-				<br />
-				<br />
-				<br />
-				<div className="footer">
-					<CheerPlayer />
-					<Contact play={play} />
-					<footer>
-						<p style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>© 2023 Ajay Ghimire</p>
-					</footer>
-				</div>
-			</div>
-		</>
-	);
+        <Routes>
+          <Route path="/" element={<Home shadow={shadow} play={play} />} />
+          <Route
+            path="/about"
+            element={<About play={play} hover={onHover} />}
+          />
+          <Route
+            path="/projects"
+            element={<Project play={play} hover={onHover} />}
+          />
+          <Route
+            path="/projects/:name"
+            element={<EachProject play={play} hover={onHover} />}
+          />
+          <Route path="/blogs" element={<Blog />} />
+          <Route
+            path="/resume"
+            element={<Resume play={play} hover={onHover} />}
+          />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+        <br />
+        <Navbar play={play} hover={onHover} />
+        <br />
+        <br />
+
+        <div className="footer">
+          <CheerPlayer />
+          <br />
+          <Contact play={play} />
+          <footer>
+            <p style={{ filter: `drop-shadow(5px -5px 7px ${shadow})` }}>
+              © 2023 Ajay Ghimire
+            </p>
+          </footer>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
--- src/components/cheer/CheerPlayer.jsx
+++ src/components/cheer/CheerPlayer.jsx
@@ -1,86 +1,95 @@
-import React, { useState } from 'react';
-import './cheer.css';
-import { useRef } from 'react';
-import { useEffect } from 'react';
-import { songsUrl } from './songs';
-import { BsPlayCircle, BsPauseCircle } from 'react-icons/bs';
-import { BiShuffle } from 'react-icons/bi';
+import React, { useState } from "react";
+import "./cheer.css";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { songsUrl } from "./songs";
+import { BsPlayCircle, BsPauseCircle } from "react-icons/bs";
+import { BiShuffle } from "react-icons/bi";
+import music from "../../images/music.jpeg"
 
 function CheerPlayer() {
-	const [cheerUp, setCheerUp] = useState(false);
-	const [isPlaying, setIsPlaying] = useState(false);
-	const [trackIndex, setTrackIndex] = useState(0);
+  const [cheerUp, setCheerUp] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [trackIndex, setTrackIndex] = useState(0);
 
-	const songRef = useRef(new Audio(songsUrl[trackIndex]));
-	const intervalRef = useRef();
-	const isReady = useRef(false);
+  const songRef = useRef(new Audio(songsUrl[trackIndex]));
+  const intervalRef = useRef();
+  const isReady = useRef(false);
 
-	const onCheerUpClick = () => {
-		setIsPlaying(true);
-		setCheerUp(true);
-	};
+  const onCheerUpClick = () => {
+    setIsPlaying(true);
+    setCheerUp(true);
+  };
 
-	const onNextClick = () => {
-		if (trackIndex < songsUrl.length - 1) {
-			setTrackIndex(trackIndex + 1);
-		} else if (trackIndex === songsUrl.length - 1) {
-			setTrackIndex(0);
-		}
-	};
+  const onNextClick = () => {
+    if (trackIndex < songsUrl.length - 1) {
+      setTrackIndex(trackIndex + 1);
+    } else if (trackIndex === songsUrl.length - 1) {
+      setTrackIndex(0);
+    }
+  };
 
-	const onPlayPauseClick = (value) => {
-		setIsPlaying(value);
-	};
+  const onPlayPauseClick = (value) => {
+    setIsPlaying(value);
+  };
 
-	useEffect(() => {
-		if (isPlaying) {
-			songRef.current.play();
-			songRef.current.volume = 0.2;
-		} else {
-			songRef.current.pause();
-		}
-	}, [isPlaying]);
+  useEffect(() => {
+    if (isPlaying) {
+      songRef.current.play();
+      songRef.current.volume = 0.2;
+    } else {
+      songRef.current.pause();
+    }
+  }, [isPlaying]);
 
-	useEffect(() => {
-		songRef.current.pause();
-		clearInterval(intervalRef.current);
-	}, []);
+  useEffect(() => {
+    songRef.current.pause();
+    clearInterval(intervalRef.current);
+  }, []);
 
-	useEffect(() => {
-		songRef.current.pause();
-		songRef.current = new Audio(songsUrl[trackIndex]);
+  useEffect(() => {
+    songRef.current.pause();
+    songRef.current = new Audio(songsUrl[trackIndex]);
 
-		if (isReady.current) {
-			songRef.current.play();
-			songRef.current.volume = 0.2;
-			setIsPlaying(true);
-		} else {
-			isReady.current = true;
-		}
-	}, [trackIndex]);
+    if (isReady.current) {
+      songRef.current.play();
+      songRef.current.volume = 0.2;
+      setIsPlaying(true);
+    } else {
+      isReady.current = true;
+    }
+  }, [trackIndex]);
 
-	return (
-		<div className="cheer-container">
-			{cheerUp ? (
-				<div className="player">
-					<div>
-						{isPlaying ? (
-							<BsPauseCircle size={30} onClick={() => onPlayPauseClick(false)} />
-						) : (
-							<BsPlayCircle size={30} onClick={() => onPlayPauseClick(true)} />
-						)}
-					</div>
-					<div>
-						<BiShuffle size={30} onClick={() => onNextClick()} />
-					</div>
-				</div>
-			) : (
-				<p className="cheer-up" onClick={onCheerUpClick}>
-					Click to Cheer Up
-				</p>
-			)}
-		</div>
-	);
+  return (
+    <div className="cheer-container">
+      {cheerUp ? (
+        <div className="player">
+          <div>
+            {isPlaying ? (
+              <BsPauseCircle
+                size={30}
+                onClick={() => onPlayPauseClick(false)}
+              />
+            ) : (
+              <BsPlayCircle size={30} onClick={() => onPlayPauseClick(true)} />
+            )}
+          </div>
+          <div>
+            <BiShuffle size={30} onClick={() => onNextClick()} />
+          </div>
+        </div>
+      ) : (
+        <div className="cheer-up" onClick={onCheerUpClick}>
+            <img
+              src={music}
+              alt="avatar"
+              className="avatar-image"
+            />
+			<button>Click Me to Cheer Up</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default CheerPlayer;
