cc9ff434321bbd980d9b7012e3488d37fe40a77d
AjayGhimire <ajayghimire1998@gmail.com>
2023-03-07 18:05:17 +1100

    added new contents

--- src/components/blogs/blogs-data.js
+++ src/components/blogs/blogs-data.js
@@ -1,37 +1,48 @@
 export const blogsData = [
-    {
-        title: "The ’final’ keyword in Java",
-        desc: "The ’final ’ keyword is a non-access modifier used for classes, attributes or variables, and methods, which makes them non-changeable, meaning impossible to inherit or override. Most common uses of ‘final ’are:....",
-        url: "https://medium.com/@ajayghimire1998/the-final-keyword-in-java-2471f9b63cb6"
-    }, 
-    {
-        title: "“Array”​ Data Structure: An Illustrative Introduction",
-        desc: "An array is a collection of items stored consecutively in the memory. An array has three main features: All items in an array must be of the same data type. An array is indexed, meaning that we can access each....",
-        url: "https://medium.com/@ajayghimire1998/array-data-structure-an-illustrative-introduction-a4f5ca8cce46"
-    },
-    {
-        title: "CRYPTOMANIA- a RUBY CLI APPLICATION",
-        desc: "Ruby was indeed the most interesting language to learn. It has all those built in methods, which are very easy and quick to use. Being one of the most promising Object-Oriented Language, Ruby provides a vast scope on....",
-        url: "https://medium.com/@ajayghimire1998/cryptomania-a-ruby-cli-application-f959f2508606"
-    },
-    {
-        title: "Was Creating a Meme App for React’s Final project worth it??",
-        desc: "Totally……This project has completely made myself feel that I am no longer a beginner but a competitive intermediate who now has guts to tackle every logical problem or at least try. With a month of learning....",
-        url: "https://medium.com/@ajayghimire1998/was-creating-a-meme-app-for-reacts-final-project-worth-it-f1859731ff6f"
-    },
-    {
-        title: "MovieFinder JS App using OMDB API",
-        desc: "Within just 4 weeks into the JavaScript course, I was poured myself with tons of course contents and structures. Coding in a real programming language for the first time was exciting as well as pretty....",
-        url: "https://medium.com/@ajayghimire1998/movie-search-js-app-using-omdb-api-93033f0898a6"
-    },
-    {
-        title: "I have created my first portfolio website",
-        desc: "Within one week into the full-time software engineering course by Academy Xi in Sydney, Australia, I have successfully created my first personal website using only HTML and CSS. It was fun and not so challenging....",
-        url: "https://medium.com/@ajayghimire1998/i-have-created-my-first-portfolio-website-4d0da88f4145"
-    },
-    {
-        title: "Why did I decide to study Software Engineering ?",
-        desc: "Computer Science is a course which has always held a certain interest to me as I loved doing various stuffs with computer and always seek for learning more in computing field. In today’s world, computer science is ubiquitous....",
-        url: "https://medium.com/@ajayghimire1998/why-did-i-decide-to-study-software-engineering-8bf843bdfb10"
-    },
-]
+  {
+    title: '"Recursive Methods" in Java ',
+    desc: "Recursive methods in Java are powerful tools for solving complex problems by breaking them down into smaller sub-problems. By calling the function on each sub-problem recursively, the solution can be computed in a clear and concise way.....",
+    url: "https://www.linkedin.com/feed/update/urn:li:activity:7037722687158255616?utm_source=share&utm_medium=member_desktop",
+  },
+  {
+    title: 'The "main" method in Java ',
+    desc: "When you write a program in Java, you need to have a special method called “main” that tells the computer where to start running the program. The main method is the entry point of aprogram. It is like the boss. It tells the....",
+    url: "https://www.linkedin.com/feed/update/urn:li:activity:7035211177932517376?utm_source=share&utm_medium=member_desktop",
+  },
+
+  {
+    title: "The ’final’ keyword in Java",
+    desc: "The ’final ’ keyword is a non-access modifier used for classes, attributes or variables, and methods, which makes them non-changeable, meaning impossible to inherit or override. Most common uses of ‘final ’are:....",
+    url: "https://medium.com/@ajayghimire1998/the-final-keyword-in-java-2471f9b63cb6",
+  },
+  {
+    title: "“Array”​ Data Structure: An Illustrative Introduction",
+    desc: "An array is a collection of items stored consecutively in the memory. An array has three main features: All items in an array must be of the same data type. An array is indexed, meaning that we can access each....",
+    url: "https://medium.com/@ajayghimire1998/array-data-structure-an-illustrative-introduction-a4f5ca8cce46",
+  },
+  {
+    title: "CRYPTOMANIA- a RUBY CLI APPLICATION",
+    desc: "Ruby was indeed the most interesting language to learn. It has all those built in methods, which are very easy and quick to use. Being one of the most promising Object-Oriented Language, Ruby provides a vast scope on....",
+    url: "https://medium.com/@ajayghimire1998/cryptomania-a-ruby-cli-application-f959f2508606",
+  },
+  {
+    title: "Was Creating a Meme App for React’s Final project worth it??",
+    desc: "Totally……This project has completely made myself feel that I am no longer a beginner but a competitive intermediate who now has guts to tackle every logical problem or at least try. With a month of learning....",
+    url: "https://medium.com/@ajayghimire1998/was-creating-a-meme-app-for-reacts-final-project-worth-it-f1859731ff6f",
+  },
+  {
+    title: "MovieFinder JS App using OMDB API",
+    desc: "Within just 4 weeks into the JavaScript course, I was poured myself with tons of course contents and structures. Coding in a real programming language for the first time was exciting as well as pretty....",
+    url: "https://medium.com/@ajayghimire1998/movie-search-js-app-using-omdb-api-93033f0898a6",
+  },
+  {
+    title: "I have created my first portfolio website",
+    desc: "Within one week into the full-time software engineering course by Academy Xi in Sydney, Australia, I have successfully created my first personal website using only HTML and CSS. It was fun and not so challenging....",
+    url: "https://medium.com/@ajayghimire1998/i-have-created-my-first-portfolio-website-4d0da88f4145",
+  },
+  {
+    title: "Why did I decide to study Software Engineering ?",
+    desc: "Computer Science is a course which has always held a certain interest to me as I loved doing various stuffs with computer and always seek for learning more in computing field. In today’s world, computer science is ubiquitous....",
+    url: "https://medium.com/@ajayghimire1998/why-did-i-decide-to-study-software-engineering-8bf843bdfb10",
+  },
+];
