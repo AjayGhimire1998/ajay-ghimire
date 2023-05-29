3ff2182282da87fff2b1a805d11f8d4ce2fca8b5
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-29 18:26:32 +1000

    fixing blog apperarance

--- src/components/blogs/BlogItem.jsx
+++ src/components/blogs/BlogItem.jsx
@@ -11,7 +11,7 @@ function BlogItem({ blog }) {
         <img src={blog.profile} alt="pp" />
         <div className="desc">
           <p>Ajay Ghimire</p>
-		  {/* < */}
+		 <span> {blog.readTime}</span>
         </div>
       </div>
 
