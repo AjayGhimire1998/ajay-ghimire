a6ea639959554b86815f9c60f055743d4841458b
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-29 18:18:38 +1000

    playing with css

--- src/components/blogs/BlogItem.jsx
+++ src/components/blogs/BlogItem.jsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import './blog-item.css';
+import React from "react";
+import "./blog-item.css";
 
 function BlogItem({ blog }) {
-	return (
-		<div className="blog-item-container" onClick={() => window.open(`${blog.url}`, '_blank')}>
-			<h4>{blog.title}</h4>
-			<p>
-				{blog.desc}
-				<a href={blog.url} target="_blank" rel="noreferrer">
-					Read More
-				</a>
-			</p>
-		</div>
-	);
+  return (
+    <div
+      className="blog-item-container"
+      onClick={() => window.open(`${blog.url}`, "_blank")}
+    >
+      <div className="imgDiv">
+        <img src={blog.profile} alt="pp" />
+      </div>
+
+      <h4>{blog.title}</h4>
+      <p>
+        {blog.desc}
+        <a href={blog.url} target="_blank" rel="noreferrer">
+          Read More
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default BlogItem;
