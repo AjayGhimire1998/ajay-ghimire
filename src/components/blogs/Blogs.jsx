37a40ea2bf103bd4013aeaee9c91fb12d92beb25
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-29 19:35:18 +1000

    looks nice actually

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -1,34 +1,28 @@
-import BlogItem from "./BlogItem";
-import "./blogs.css";
-import { blogsData } from "./blogs-data";
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import BlogItem from './BlogItem';
+import './blogs.css';
+import { blogsData } from './blogs-data';
+import { useState } from 'react';
+import {v4 as uuidv4} from "uuid"
 
 function Blogs() {
-  const [blogCount, setBlogCount] = useState(4);
-  const blogItem = blogsData.map((blog) => {
-    return <BlogItem blog={blog} key={uuidv4()} />;
-  });
+	const [blogCount, setBlogCount] = useState(4);
+	const blogItem = blogsData.map((blog) => {
+		return <BlogItem blog={blog} key={uuidv4()}/>;
+	});
 
-  const onSeeMore = () => {
-    setBlogCount(blogCount + 4);
-  };
+	const onSeeMore = () => {
+		setBlogCount(blogCount + 4);
+	};
 
-  const onSeeLess = () => {
-    setBlogCount(4);
-  };
-  return (
-    <>
-      <div className="blogs-container">{blogItem.slice(0, blogCount)}</div>
-      <div className="view">
-        {blogCount === 4 ? (
-          <p onClick={onSeeMore}>See More</p>
-        ) : (
-          <p onClick={onSeeLess}>See Less</p>
-        )}
-      </div>
-    </>
-  );
+	const onSeeLess = () => {
+		setBlogCount(4);
+	};
+	return (
+		<div className="blogs-container">
+			{blogItem.slice(0, blogCount)}
+			{blogCount === 4 ? <p onClick={onSeeMore}>See More</p> : <p onClick={onSeeLess}>See Less</p>}
+		</div>
+	);
 }
 
 export default Blogs;
