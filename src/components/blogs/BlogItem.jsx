13d672ce084abffa3fbd11357c05a0b86bd40e22
AjayGhimire <ajayghimire1998@gmail.com>
2023-05-31 22:13:49 +1000

    looks fine

--- src/components/blogs/BlogItem.jsx
+++ src/components/blogs/BlogItem.jsx
@@ -27,39 +27,41 @@ function BlogItem({ blog, hover, play }) {
             <span> {blogReadAndDatea} </span>
           </div>
         </div>
-        <div className="blog-contents">
-          <div className="tag">
-            <span>{blog.tag}</span>
-          </div>
-
-          <h4>{blog.title}</h4>
-          {isOpen ? (
-            <PopUp
-              blog={blog}
-              play={play}
-              hover={hover}
-              isOpen={isOpen}
-              closeModal={closeModal}
-            />
-          ) : (
-            <p>
-              {blog.desc}
-              <span
-                style={
-                  isHover
-                    ? { color: "#39ff14", fontSize: "large", cursor: "pointer" }
-                    : { animation: "changeText 8s linear infinite" }
-                }
-                onMouseOver={() => setIsHover(true)}
-                onMouseLeave={() => setIsHover(false)}
-                onClick={isOpenClick}
-              >
-                Read More
-              </span>
-            </p>
-          )}
+        {/* <div className="blog-contents"> */}
+        <div className="tag">
+          <span>{blog.tag}</span>
         </div>
+
+        <br />
+        <br/>
+        <h4>{blog.title}</h4>
+        {isOpen ? (
+          <PopUp
+            blog={blog}
+            play={play}
+            hover={hover}
+            isOpen={isOpen}
+            closeModal={closeModal}
+          />
+        ) : (
+          <p>
+            {blog.desc}
+            <span
+              style={
+                isHover
+                  ? { color: "#39ff14", fontSize: "large", cursor: "pointer" }
+                  : { animation: "changeText 8s linear infinite" }
+              }
+              onMouseOver={() => setIsHover(true)}
+              onMouseLeave={() => setIsHover(false)}
+              onClick={isOpenClick}
+            >
+              Read More
+            </span>
+          </p>
+        )}
       </div>
+      {/* </div> */}
     </>
   );
 }
