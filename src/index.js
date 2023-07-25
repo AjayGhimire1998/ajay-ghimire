9cf74cdd81ffa6bf6e7c3c9e9b852fdd4acd9698
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-07-25 17:34:57 +1000

    fixing the routing spa bug

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="https://ajayghimire.com">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
