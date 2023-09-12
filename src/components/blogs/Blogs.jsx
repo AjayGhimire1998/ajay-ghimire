b198767dd495a875939e037477576e059441c493
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-12 12:39:53 +1000

    filter adding later

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -4,16 +4,12 @@ import { blogsData } from "./blogs-data";
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
-
 function Blogs({ play, hover }) {
   const [blogCount, setBlogCount] = useState(5);
-
 
   const blogItem = blogsData.map((blog) => {
     return <BlogItem blog={blog} key={uuidv4()} play={play} hover={hover} />;
   });
-
-
 
   const onSeeMore = () => {
     setBlogCount(blogsData.length);
@@ -24,23 +20,19 @@ function Blogs({ play, hover }) {
   };
   return (
     <>
-    
-    <div className="blogs-container">
-      <div className="page-head">
-        <h3>
-          <span>Blogs</span>
-        </h3>
-      </div>
-     
-      {blogItem.slice(0, blogCount)}
-    
-      {blogCount === 5 ? (
-        <p onClick={onSeeMore} >See More</p>
-      ) : (
-        <p onClick={onSeeLess}>See Less</p>
-      )}
-    </div>
+      <div className="blogs-container">
+        <div className="page-head">
+          <h3>Blogs</h3>
+        </div>
+        {/* <div>Filter</div> */}
+        {blogItem.slice(0, blogCount)}
 
+        {blogCount === 5 ? (
+          <p onClick={onSeeMore}>See More</p>
+        ) : (
+          <p onClick={onSeeLess}>See Less</p>
+        )}
+      </div>
     </>
   );
 }
