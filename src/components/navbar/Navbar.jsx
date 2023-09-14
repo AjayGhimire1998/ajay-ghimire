039e39e8e98ab4123cc22f99777e645347612cb7
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-14 17:57:17 +1000

    navbar as it it

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,68 +1,78 @@
-import React, { useEffect, useState } from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-
-import './navbar.css';
+import "./navbar.css";
 function Navbar({ play, hover }) {
-	const location = useLocation();
-	const [currentPage, setCurrentPage] = useState('');
-	const [pages, setPages] = useState(['About', 'Projects', 'Contents', 'Resume']);
+  const location = useLocation();
+  const [currentPage, setCurrentPage] = useState("");
+  const [pages, setPages] = useState([
+    "About",
+    "Projects",
+    "Contents",
+    "Resume",
+  ]);
 
-	const playSound = () => {
-		play();
-	};
-	const onLinkClick = (page) => {
-		setCurrentPage(page);
-		localStorage.setItem('current_page', JSON.stringify(page));
-	};
-	useEffect(() => {
-		localStorage.setItem(
-			'current_page',
-			JSON.stringify(location.pathname.slice(1).charAt(0).toUpperCase() + location.pathname.slice(2))
-		);
-		setCurrentPage(JSON.parse(localStorage.getItem('current_page')));
-	}, [location.pathname]);
+  const playSound = () => {
+    play();
+  };
+  const onLinkClick = (page) => {
+    setCurrentPage(page);
+    localStorage.setItem("current_page", JSON.stringify(page));
+  };
+  useEffect(() => {
+    localStorage.setItem(
+      "current_page",
+      JSON.stringify(
+        location.pathname.slice(1).charAt(0).toUpperCase() +
+          location.pathname.slice(2)
+      )
+    );
+    setCurrentPage(JSON.parse(localStorage.getItem("current_page")));
+  }, [location.pathname]);
 
-	useEffect(() => {
-		if (currentPage === '') {
-			setPages(['About', 'Projects', 'Blogs', 'Resume']);
-		}
-		if (currentPage === 'About') {
-			setPages(['Home', 'Projects', 'Blogs', 'Resume']);
-		}
-		if (currentPage === 'Projects') {
-			setPages(['Home', 'About', 'Blogs', 'Resume']);
-		}
-		if (currentPage === 'Blogs') {
-			setPages(['Home', 'About', 'Projects', 'Resume']);
-		}
-		if (currentPage === 'Resume') {
-			setPages(['Home', 'About', 'Projects', 'Blogs']);
-		}
-	}, [currentPage]);
+  useEffect(() => {
+    if (currentPage === "") {
+      setPages(["About", "Projects", "Blogs", "Resume"]);
+    }
+    if (currentPage === "About") {
+      setPages(["Home", "Projects", "Blogs", "Resume"]);
+    }
+    if (currentPage === "Projects") {
+      setPages(["Home", "About", "Blogs", "Resume"]);
+    }
+    if (currentPage === "Blogs") {
+      setPages(["Home", "About", "Projects", "Resume"]);
+    }
+    if (currentPage === "Resume") {
+      setPages(["Home", "About", "Projects", "Blogs"]);
+    }
+  }, [currentPage]);
 
-	return (
-		<div className="nav-container">
-			<div className="navbar">
-				{pages.map((page, index) => {
-					return (
-						<Link
-							to={page === 'Home' ? '' : `/${page.toLowerCase()}`}
-							className="nav-button"
-							onClick={() => {
-								onLinkClick(page === 'Home' ? '' : page);
-								playSound();
-							}}
-							onMouseOver={hover}
-							key={index}
-						>
-							{page}
-						</Link>
-					);
-				})}
-			</div>
-		</div>
-	);
+  return (
+    <nav className="nav-container">
+      <div className="navbar">
+        {pages.map((page, index) => {
+          return (
+            // <li key={index}>
+              <Link
+                to={page === "Home" ? "" : `/${page.toLowerCase()}`}
+                className={`nav-button ${page.toLowerCase() === currentPage.toLowerCase() ? 'active' : ''}`}
+                onClick={() => {
+                  onLinkClick(page === "Home" ? "" : page);
+                  playSound();
+                }}
+                onMouseOver={hover}
+				role="menuitem"
+				tabIndex={0}
+              >
+                {page}
+              </Link>
+            // </li>
+          );
+        })}
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
