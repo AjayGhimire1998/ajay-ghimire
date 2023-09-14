724d4327b464b1c3929ba7ed9abdfc7efbb8fb97
Ajay Ghimire <73376772+AjayGhimire1998@users.noreply.github.com>
2023-09-14 11:46:03 +1000

    added reset button

--- src/components/blogs/Blogs.jsx
+++ src/components/blogs/Blogs.jsx
@@ -18,6 +18,11 @@ function Blogs({ play, hover }) {
     }
   };
 
+  const handleResetAll = () => {
+    setIsFilterOn(false);
+    setSelectedCategories([]);
+  }
+
   useEffect(() => {
     console.log(selectedCategories);
   }, [selectedCategories]);
@@ -48,18 +53,56 @@ function Blogs({ play, hover }) {
           <h3>Blogs</h3>
         </div>
         <section className="filter-wrapper">
-          <div className="filter-button">
-            <p>
-              Filter &nbsp;&nbsp;
-              {!isFilterOn ? (
-                <i
-                  className="arrow-down"
+          <div className="filter-button-wrapper">
+            <div className="filter-button">
+              <p>
+                Filter &nbsp;&nbsp;
+                <small
                   onClick={() => setIsFilterOn(!isFilterOn)}
-                ></i>
-              ) : (
-                <small onClick={() => setIsFilterOn(!isFilterOn)} className="close">x</small>
-              )}
-            </p>
+                  className="arrow"
+                >
+                  {isFilterOn ? "x" : ">"}
+                </small>
+              </p>
+            </div>
+            {selectedCategories.length > 0 ? (
+              <div className="reset" onClick={handleResetAll}>
+                <p>Reset &nbsp;</p>{" "}
+                <svg
+                  width="15px"
+                  height="15px"
+                  viewBox="0 0 21 21"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="white"
+                >
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                  <g
+                    id="SVGRepo_tracerCarrier"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></g>
+                  <g id="SVGRepo_iconCarrier">
+                    {" "}
+                    <g
+                      fill="none"
+                      fillRule="evenodd"
+                      stroke="white"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      transform="matrix(0 1 1 0 2.5 2.5)"
+                      strokeWidth="2"
+                    >
+                      {" "}
+                      <path d="m3.98652376 1.07807068c-2.38377179 1.38514556-3.98652376 3.96636605-3.98652376 6.92192932 0 4.418278 3.581722 8 8 8s8-3.581722 8-8-3.581722-8-8-8"></path>{" "}
+                      <path
+                        d="m4 1v4h-4"
+                        transform="matrix(1 0 0 -1 0 6)"
+                      ></path>{" "}
+                    </g>{" "}
+                  </g>
+                </svg>
+              </div>
+            ) : null}
           </div>
           <br />
           {isFilterOn ? (
